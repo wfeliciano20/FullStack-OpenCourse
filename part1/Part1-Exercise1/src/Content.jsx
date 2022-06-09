3c5b9b273b79b041b41f4edc5cf542195e3e2ec2
william a feliciano rios <william.feliciano2@gmail.com>
2022-06-08 22:56:13 -0400

    updated Content.jsx for exercises 1&2

--- part1/Part1-Exercise1/src/Content.jsx
+++ part1/Part1-Exercise1/src/Content.jsx
@@ -1,7 +1,7 @@
 
 const Content = ({part1, part2, part3, exercises1, exercises2, exercises3}) => {
   return (
-    <>
+    <div>
       <p>
         {part1} {exercises1}
       </p>
@@ -11,7 +11,7 @@ const Content = ({part1, part2, part3, exercises1, exercises2, exercises3}) => {
       <p>
         {part3} {exercises3}
       </p>
-    </>
+    </div>
   )
 }
 
--- part1/Part1-Exercise2/part1-exercise2/src/Content.jsx
+++ part1/Part1-Exercise2/part1-exercise2/src/Content.jsx
@@ -2,11 +2,11 @@ import Part from './Part';
 
 const Content = ({part1, part2, part3, exercises1, exercises2, exercises3}) => {
   return (
-    <>
+    <div>
       <Part partName={part1} partExercise={exercises1} />
       <Part partName={part2} partExercise={exercises2} />
       <Part partName={part3} partExercise={exercises3} />
-    </>
+    </div>
   )
 }
 
