59caf05c04ec7b73d1e07360b3c67d3d4a0c0997
william a feliciano rios <william.feliciano2@gmail.com>
2022-06-11 19:39:20 -0400

    part2b-ex6 full code

---
+++ Part2/PartB/Part2B-Exercise6/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]);
+  const [newName, setNewName] = useState('');
+
+  const handleOnSubmit = e =>{
+    e.preventDefault();
+    setPersons(persons.concat({ name: newName }));
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleOnSubmit}>
+        <div>
+          name: <input value={newName} onChange={(e)=>setNewName(e.target.value)} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person =>{
+        return <p key={person.name}>{person.name}</p>
+      })}
+    </div>
+  )
+}
+
+export default App;
