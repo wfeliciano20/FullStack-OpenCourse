3df98d175542a862cdfef83c74f1de098592e0ad
william a feliciano rios <william.feliciano2@gmail.com>
2022-06-10 22:35:54 -0400

    ex13 full code

--- Part1/PartD/Part1D-Exercise13/src/App.jsx
+++ Part1/PartD/Part1D-Exercise13/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
 const App = () => {
   const anecdotes = [
@@ -9,18 +9,27 @@ const App = () => {
     'Premature optimization is the root of all evil.',
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
-  ]
+  ];
+
   const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0));
 
   const getNextAnecdote = (e) => {
     const random = Math.floor(Math.random() * anecdotes.length);
     setSelected(random);
   }
+  const handleVote = (e) => {
+    const updatedVotes = [...votes];
+    updatedVotes[selected]= votes[selected] + 1;
+    setVotes(updatedVotes);
+  }
 
   return (
     <div>
       {anecdotes[selected]}
+      <p>Has {votes[selected]} votes</p>
       <div>
+        <button onClick={handleVote}>vote</button>
         <button onClick={getNextAnecdote}>next anecdote</button>
       </div>
       
