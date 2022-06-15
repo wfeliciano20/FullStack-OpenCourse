e744f44a7f746db6c2298ce2e1d090f0eb1ca758
william a feliciano rios <william.feliciano2@gmail.com>
2022-06-15 18:27:56 -0400

    part3d-exercise22 full code

---
+++ Part3/PartD/Part3D-Exercise22/models/person.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose')
+
+// eslint-disable-next-line no-undef
+const MONGO_URI = process.env.MONGO_URI
+
+
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error'))
+db.once('open', () => {
+  console.log('Connected successfully')
+})
+
+const personSchema = new mongoose.Schema({
+  id: {
+    type: Number,
+    required: true,
+  },
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: function(v) {
+        return /^\d{2}-\d{7}$/.test(v) || /^\d{3}-\d{8}$/.test(v)
+      },
+      message: props => `${props.value} is not a valid phone number! use one of this ##-####### or ###-########`
+    },
+    required: true
+  },
+})
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject._id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Person', personSchema)
