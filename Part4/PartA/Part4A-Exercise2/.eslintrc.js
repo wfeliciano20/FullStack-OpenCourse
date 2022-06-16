30f35c12ef3a0178a6c8ea046e541c16d94862e1
william a feliciano rios <william.feliciano2@gmail.com>
2022-06-15 22:50:39 -0400

    part4a-exercise2 utils done

---
+++ Part4/PartA/Part4A-Exercise2/.eslintrc.js
@@ -0,0 +1,18 @@
+/* eslint-disable indent */
+module.exports = {
+    env: {
+        browser: true,
+        commonjs: true,
+        es2021: true,
+    },
+    extends: ['eslint:recommended'],
+    parserOptions: {
+        ecmaVersion: 'latest',
+    },
+    rules: {
+        indent: ['error', 2],
+        'linebreak-style': ['error', 'unix'],
+        quotes: ['error', 'single'],
+        semi: ['error', 'never'],
+    },
+}
