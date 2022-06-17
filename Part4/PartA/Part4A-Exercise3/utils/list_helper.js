657ddf113565046e09465a406ea3b30aa993cb94
william a feliciano rios <william.feliciano2@gmail.com>
2022-06-17 00:22:08 -0400

    part4a-exercise3 full code updated

--- Part4/PartA/Part4A-Exercise3/utils/list_helper.js
+++ Part4/PartA/Part4A-Exercise3/utils/list_helper.js
@@ -3,10 +3,5 @@ const dummy = (blogs) => {
     return 1;
 };
 
-const totalLikes = (blogs) => {
-    return blogs.reduce((sum, blog) => {
-        return sum + blog.likes;
-    }, 0);
-};
 
-module.exports = { dummy, totalLikes };
+module.exports = { dummy };
