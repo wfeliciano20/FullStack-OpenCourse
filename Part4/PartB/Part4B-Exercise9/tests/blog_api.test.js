e57893b9ea1b8b90d0b78a4fb63b8af61e63dbaf
william a feliciano rios <william.feliciano2@gmail.com>
2022-06-17 23:23:44 -0400

    part4b-exercise9 full code

---
+++ Part4/PartB/Part4B-Exercise9/tests/blog_api.test.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose');
+const supertest = require('supertest');
+const app = require('../app');
+const Blog = require('../models/blog');
+const api = supertest(app);
+
+const initialBlogs = [
+    {
+        title: 'Learning Mern',
+        author: 'John Doe',
+        url: 'http://localhost:3003/api/blogs',
+        likes:10
+    },
+    {
+        title: 'Learning Express',
+        author: 'Jane Doe',
+        url: 'http://localhost:3003/api/blogs',
+        likes:20
+    }
+];
+
+beforeEach(async () => {
+    await Blog.deleteMany({});
+    let BlogObject = new Blog(initialBlogs[0]);
+    await BlogObject.save();
+    BlogObject = new Blog(initialBlogs[1]);
+    await BlogObject.save();
+});
+
+test('blogs are returned as json', async () => {
+    await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/);
+});
+
+test('there are two blogs', async () => {
+    const response = await api.get('/api/blogs');
+    expect(response.body).toHaveLength(initialBlogs.length);
+});
+
+test('the first Blog is about Learning Mern', async () => {
+    const response = await api.get('/api/Blogs');
+
+    const titles = response.body.map(r => r.title);
+    expect(titles).toContain(
+        'Learning Mern'
+    );
+});
+
+test('check the blogs have an id', async () => {
+    const response = await api.get('/api/Blogs');
+
+    const blogs = response.body.map(r => r);
+    // expect ids elements havve property id
+    expect(blogs[0].id).toBeDefined();
+});
+
+afterAll(() => {
+    mongoose.connection.close();
+});
