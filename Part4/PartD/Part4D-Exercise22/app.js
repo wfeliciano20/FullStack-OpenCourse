7e2972ed984ad13078c736a821f47d4dabe72b78
william a feliciano rios <william.feliciano2@gmail.com>
2022-06-23 19:40:51 -0400

    part4d-exercise22 full code

---
+++ Part4/PartD/Part4D-Exercise22/app.js
@@ -0,0 +1,39 @@
+const config = require('./utils/config');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const loginRouter = require('./controllers/login');
+const blogsRouter = require('./controllers/blogs');
+const usersRouter = require('./controllers/users');
+const middleware = require('./utils/middleware');
+const logger = require('./utils/loggers');
+const mongoose = require('mongoose');
+
+logger.info('connecting to db...');
+
+// eslint-disable-next-line no-undef
+mongoose.connect(config.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', (err) =>
+  logger.error('error connecting to MongoDB:', err.message),
+);
+db.once('open', () => {
+  logger.info('connected to MongoDB');
+});
+app.use(middleware.tokenExtractor);
+app.use(cors());
+app.use(express.static('build'));
+app.use(express.json());
+app.use(middleware.requestLogger);
+app.use('/api/users', usersRouter);
+app.use('/api/blogs', middleware.userExtractor, blogsRouter);
+app.use('/api/login', loginRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
---
+++ Part4/PartD/Part4D-Exercise22/controllers/login.js
@@ -0,0 +1,33 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const config = require('../utils/config');
+const loginRouter = require('express').Router();
+const User = require('../models/user');
+
+loginRouter.post('/', async (request, response) => {
+  const { username, password } = request.body;
+  const user = await User.findOne({ username });
+
+  const passwordCorrect =
+    user === null ? false : bcrypt.compareSync(password, user.passwordHash);
+
+  if (!(user && passwordCorrect)) {
+    return response.status(401).json({
+      error: 'invalid username or password',
+    });
+  }
+
+  const userForToken = {
+    username: user.username,
+    id: user._id,
+  };
+
+  // eslint-disable-next-line no-undef
+  const token = jwt.sign(userForToken, config.SECRET, { expiresIn: 60 * 60 });
+
+  response
+    .status(200)
+    .send({ token, username: user.username, name: user.name });
+});
+
+module.exports = loginRouter;
