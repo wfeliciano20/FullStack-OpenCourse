414a76fe2b4316aaae4e7a428cca9a1771c3f543
william a feliciano rios <william.feliciano2@gmail.com>
2022-07-14 14:42:02 -0400

    part5c-exercise16 full code

---
+++ Part5/PartC/Part5C-Exercise16/BloglistFrontend/src/components/LoginForm.jsx
@@ -0,0 +1,53 @@
+import PropTypes from 'prop-types';
+// eslint-disable-next-line no-unused-vars
+import Notification from './Notification';
+
+const LoginForm = ({
+	username,
+	setUsername,
+	password,
+	setPassword,
+	handleLogin,
+	notification,
+	notificationType,
+}) => {
+	return (
+		<div>
+			<Notification message={notification} type={notificationType} />
+			<form onSubmit={handleLogin}>
+				<div>
+					<h1>Login</h1>
+					username
+					<input
+						type="text"
+						value={username}
+						name="Username"
+						onChange={({ target }) => setUsername(target.value)}
+					/>
+				</div>
+				<div>
+					password
+					<input
+						type="password"
+						value={password}
+						name="Password"
+						onChange={({ target }) => setPassword(target.value)}
+					/>
+				</div>
+				<button type="submit">login</button>
+			</form>
+		</div>
+	);
+};
+
+LoginForm.propTypes = {
+	handleLogin: PropTypes.func.isRequired,
+	setUsername: PropTypes.func.isRequired,
+	setPassword: PropTypes.func.isRequired,
+	username: PropTypes.string.isRequired,
+	password: PropTypes.string.isRequired,
+	notification: PropTypes.string.isRequired,
+	notificationType: PropTypes.string.isRequired,
+};
+
+export default LoginForm;
