034fe864f5395151bd46096fb1abd1b13c45d4e6
william a feliciano rios <william.feliciano2@gmail.com>
2022-07-19 22:49:38 -0400

    part5d-exercise18 full code updated

--- Part5/PartD/Part5D-Exercise18/BloglistFrontend/cypress/e2e/blog_app.spec.cy.js
+++ Part5/PartD/Part5D-Exercise18/BloglistFrontend/cypress/e2e/blog_app.spec.cy.js
@@ -10,16 +10,40 @@ describe('Test initial loading of page', () => {
 		cy.request('POST', 'http://localhost:3003/api/users/', user);
 		cy.visit('http://localhost:3000');
 	});
+
 	it('The initial screen has the log in form', () => {
 		cy.contains('Login');
 		cy.contains('username');
 		cy.contains('password');
 	});
+});
+
+describe('Login', () => {
+	beforeEach(() => {
+		cy.request('POST', 'http://localhost:3003/api/testing/reset');
+		const user = {
+			username: 'jdoe',
+			name: 'John Doe',
+			password: '12345',
+		};
+		cy.request('POST', 'http://localhost:3003/api/users/', user);
+		cy.visit('http://localhost:3000');
+	});
+
 	it('The user can login', () => {
 		cy.get('#username').type('jdoe');
 		cy.get('#password').type('12345');
 		cy.get('#login-button').click();
 
 		cy.contains('John Doe has logged in');
+	});
+
+	// eslint-disable-next-line prettier/prettier
+	it('The user can\'t login with bad credentials', () => {
+		cy.get('#username').type('jdoe');
+		cy.get('#password').type('123456');
+		cy.get('#login-button').click();
+
+		cy.contains('invalid username or password');
 	});
 });
