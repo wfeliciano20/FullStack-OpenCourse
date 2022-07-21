b5e8714b720822b4ffa14cc3b04939f74a8cc90b
william a feliciano rios <william.feliciano2@gmail.com>
2022-07-20 21:10:09 -0400

    part6a-exercise3 full code

---
+++ Part6/PartA/Part6A-Exercise3/src/App.js
@@ -0,0 +1,42 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { voteForAnecdote, addNewAnecdote } from './reducers/anecdoteReducer';
+
+const App = () => {
+	const anecdotes = useSelector((state) => state);
+	const dispatch = useDispatch();
+
+	const vote = (id) => {
+		dispatch(voteForAnecdote(id));
+	};
+
+	const addAnecdote = (event) => {
+		event.preventDefault();
+		const content = event.target.anecdote.value;
+		event.target.anecdote.value = '';
+		dispatch(addNewAnecdote(content));
+	};
+
+	return (
+		<div>
+			<h2>Anecdotes</h2>
+			{anecdotes.map((anecdote) => (
+				<div key={anecdote.id}>
+					<div>{anecdote.content}</div>
+					<div>
+						has {anecdote.votes}
+						<button onClick={() => vote(anecdote.id)}>vote</button>
+					</div>
+				</div>
+			))}
+			<h2>create new</h2>
+			<form onSubmit={addAnecdote}>
+				<div>
+					<input name="anecdote" />
+				</div>
+				<button type="submit">create</button>
+			</form>
+		</div>
+	);
+};
+
+export default App;
