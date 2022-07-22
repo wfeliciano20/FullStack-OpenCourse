5405f2a7c706c296ae239a5bb4ca42285d2c5b09
william a feliciano rios <william.feliciano2@gmail.com>
2022-07-21 22:04:31 -0400

    part6c-exercise15 full code updated

--- Part6/PartC/Part6C-Exercise15/src/reducers/anecdotesSlice.js
+++ Part6/PartC/Part6C-Exercise15/src/reducers/anecdotesSlice.js
@@ -1,8 +1,6 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-
-
 const baseUrl = 'http://localhost:3001/anecdotes';
 
 const getId = () => (100000 * Math.random()).toFixed(0);
@@ -16,23 +14,23 @@ const asObject = (anecdote) => {
 };
 
 export const fetchAnecdotes = createAsyncThunk(
-	'filter/fetchAnecdotes',
-	async () => {
-		const response = await axios.get(baseUrl);
-		return response.data;
-	}
+    'anecdotes/fetchAnecdotes',
+    async() => {
+        const response = await axios.get(baseUrl);
+        return response.data;
+    }
 );
 
 export const saveAnecdoteToDb = createAsyncThunk(
-    'filter/saveAnecdoteToDb',
-    async (anecdote) => {
+    'anecdotes/saveAnecdoteToDb',
+    async(anecdote) => {
         const response = await axios.post(baseUrl, asObject(anecdote));
         return response.data;
     }
 );
 
 const initialState = {
-    anecdotes: []
+    anecdotes: [],
 };
 
 const anecdotesSlice = createSlice({
@@ -44,28 +42,31 @@ const anecdotesSlice = createSlice({
         },
         voteForAnecdote: (state, action) => {
             const id = action.payload;
-            
-            state.anecdotes = state.anecdotes.map((anecdote) => anecdote.id ===id ?
-                    {
-                        ...anecdote,
-                        votes: anecdote.votes + 1
-                    }
-                    :anecdote)
-        }
+
+            state.anecdotes = state.anecdotes.map((anecdote) =>
+                anecdote.id === id ?
+                {
+                    ...anecdote,
+                    votes: anecdote.votes + 1,
+                } :
+                anecdote
+            );
+        },
     },
     extraReducers: {
         [fetchAnecdotes.fulfilled]: (state, action) => {
             state.anecdotes = action.payload;
-        }, 
+        },
         [saveAnecdoteToDb.fulfilled]: (state, action) => {
             state.anecdotes.push(action.payload);
-        }
-    }
+        },
+    },
 });
 
 export const { addNewAnecdote, voteForAnecdote } = anecdotesSlice.actions;
 
 export const selectAnecdotes = (state) => {
-    return state.anecdotes.anecdotes};
+    return state.anecdotes.anecdotes;
+};
 
 export default anecdotesSlice.reducer;
