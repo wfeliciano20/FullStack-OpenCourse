0e74208918d067def0b84bc25f14255c5a79119d
William A Feliciano Rios <31602072+wfeliciano20@users.noreply.github.com>
2023-02-11 18:56:12 -0500

    Part8B-Exercise10 full code

---
+++ Part8/PartB/Part8B-Exercise10/booksFrontend/src/components/Authors.js
@@ -0,0 +1,44 @@
+import { useQuery } from '@apollo/client';
+import { ALL_AUTHORS } from '../Queries';
+
+const Authors = (props) => {
+	const { loading, error, data } = useQuery(ALL_AUTHORS);
+
+	if (!props.show) {
+		return null;
+	}
+
+	if (loading) {
+		return <p>loading...</p>;
+	}
+
+	if (error) {
+		return <p>error...{error.message}</p>;
+	}
+
+	const authors = data.allAuthors;
+
+	return (
+		<div>
+			<h2>authors</h2>
+			<table>
+				<tbody>
+					<tr>
+						<th></th>
+						<th>born</th>
+						<th>books</th>
+					</tr>
+					{authors.map((a) => (
+						<tr key={a.name}>
+							<td>{a.name}</td>
+							<td>{a.born}</td>
+							<td>{a.bookCount}</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</div>
+	);
+};
+
+export default Authors;
