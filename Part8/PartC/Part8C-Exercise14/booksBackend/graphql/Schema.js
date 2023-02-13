86ddb8a8fd918099a652f96f30a8d4de28a5d209
William A Feliciano Rios <31602072+wfeliciano20@users.noreply.github.com>
2023-02-13 12:53:36 -0500

    Part8C-Exercise14 full code

---
+++ Part8/PartC/Part8C-Exercise14/booksBackend/graphql/Schema.js
@@ -0,0 +1,35 @@
+const typeDefs = `
+	type Book {
+		title: String!
+		published: Int!
+		author: Author!
+		id: ID!
+		genres: [String!]
+	}
+
+	type Author {
+		name: String!
+		id: ID!
+		born: Int
+		bookCount: Int
+	}
+
+	type Query {
+		bookCount: Int!
+		authorCount: Int!
+		allBooks(author: String, genre: String): [Book!]
+		allAuthors: [Author!]
+	}
+
+	type Mutation {
+		addBook(
+			title: String!
+			published: Int!
+			author: String!
+			genres: [String!]!
+		): Book
+		editAuthor(name: String!, setBornTo: Int!): Author
+	}
+`;
+
+module.exports = typeDefs;
