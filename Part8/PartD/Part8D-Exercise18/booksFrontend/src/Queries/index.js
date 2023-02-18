46f1d19235ee3b62a916d80c6483ce5b2f078a8b
William A Feliciano Rios <31602072+wfeliciano20@users.noreply.github.com>
2023-02-17 22:42:40 -0500

    Part8D-Exercise18 full code

---
+++ Part8/PartD/Part8D-Exercise18/booksFrontend/src/Queries/index.js
@@ -0,0 +1,63 @@
+import { gql } from '@apollo/client';
+
+export const ALL_AUTHORS = gql`
+	query {
+		allAuthors {
+			name
+			born
+			bookCount
+		}
+	}
+`;
+
+export const ALL_BOOKS = gql`
+	query allBooks($author: String, $genre: String) {
+		allBooks(author: $author, genre: $genre) {
+			title
+			author {
+				name
+			}
+			published
+		}
+	}
+`;
+
+export const ADD_BOOK = gql`
+	mutation createBook(
+		$title: String!
+		$published: Int!
+		$author: String!
+		$genres: [String!]!
+	) {
+		addBook(
+			title: $title
+			published: $published
+			author: $author
+			genres: $genres
+		) {
+			title
+			published
+			author {
+				name
+			}
+			genres
+		}
+	}
+`;
+
+export const UPDATE_AUTHOR = gql`
+	mutation updateAuthor($name: String!, $setBornTo: Int!) {
+		editAuthor(name: $name, setBornTo: $setBornTo) {
+			name
+			born
+		}
+	}
+`;
+
+export const LOGIN = gql`
+	mutation login($username: String!, $password: String!) {
+		login(username: $username, password: $password) {
+			value
+		}
+	}
+`;
---
+++ Part8/PartD/Part8D-Exercise18/booksFrontend/src/components/Books.js
@@ -0,0 +1,39 @@
+import { useQuery } from '@apollo/client';
+import { ALL_BOOKS } from '../Queries';
+
+const Books = (props) => {
+	const { loading, error, data } = useQuery(ALL_BOOKS);
+	if (!props.show) {
+		return null;
+	}
+
+	if (loading) return <p>Loading...</p>;
+	if (error) return <p>Error :{error.message}</p>;
+
+	const books = data.allBooks;
+
+	return (
+		<div>
+			<h2>books</h2>
+
+			<table>
+				<tbody>
+					<tr>
+						<th></th>
+						<th>author</th>
+						<th>published</th>
+					</tr>
+					{books.map((a) => (
+						<tr key={a.title}>
+							<td>{a.title}</td>
+							<td>{a.author.name}</td>
+							<td>{a.published}</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</div>
+	);
+};
+
+export default Books;
